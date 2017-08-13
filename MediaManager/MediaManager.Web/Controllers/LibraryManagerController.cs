cd60fcb5614693c01b933cdd9f4451d64c77b450
RichardSimons <RichardSimons@live.com>
2017-08-12 19:58:18 -0600

    Started added MPAA Rating Controler:

--- MediaManager/MediaManager.Web/Controllers/LibraryManagerController.cs
+++ MediaManager/MediaManager.Web/Controllers/LibraryManagerController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using MediaManager.Web.Models;
 
 namespace MediaManager.Web.Controllers
 {
+    
     public class LibraryManagerController : Controller
     {
+        MediaManagerEntities Db = new MediaManagerEntities();
+
         public ActionResult Index()
         {
+
+            return View(Db.Movies);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.MPAARatingId = new SelectList(Db.MPAARatings, "MPAARatingId", "Rating");
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Movie myMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                Db.Entry(myMovie).State = EntityState.Added;
+                Db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MPAARatingId = new SelectList(Db.MPAARatings, "MPAARatingId", "Rating");
+            return View(myMovie);
         }
     }
 }
